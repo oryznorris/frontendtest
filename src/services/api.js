8dcb32553e54a894b154cf2485c6f5a2aa847957
oryznorris <oryznorris@gmail.com>
2025-01-24 11:57:32 -0300

    asdas

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,9 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 // Criação de uma instância do Axios com a URL base
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 // Exemplo de requisição
 api.get('/usuarios')
